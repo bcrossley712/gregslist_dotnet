1a4b464c5ea1c1ffc1ff413a053e88291975fc1f
Brian Crossley <bcrossley712@gmail.com>
2022-04-05 16:09:11 -0600

    cars done

--- Models/Car.cs
+++ Models/Car.cs
@@ -6,17 +6,17 @@ namespace gregslist_dotnet.Models
   public class Car
   {
 
-    public string Id { get; set; }
-    public string Make { get; set; }
-    public string Model { get; set; }
+    public string? Id { get; set; }
+    public string? Make { get; set; }
+    public string? Model { get; set; }
     [Range(1900, 2023)]
-    public int Year { get; set; }
+    public int? Year { get; set; }
     [Range(0, 20000000)]
-    public int Price { get; set; }
-    public string Description { get; set; }
-    public string Color { get; set; }
-    public string ImgUrl { get; set; }
-    public Car(string make, string model, int year, int price, string description, string color, string imgUrl)
+    public int? Price { get; set; }
+    public string? Description { get; set; }
+    public string? Color { get; set; }
+    public string? ImgUrl { get; set; }
+    public Car(string? make, string? model, int? year, int? price, string? description, string? color, string? imgUrl)
     {
       Id = Guid.NewGuid().ToString();
       Make = make;
